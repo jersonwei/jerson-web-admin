3f51d6d95969c6bad923504f422d4fd4049e5c23
jesonwei <872937094@qq.com>
2023-02-23 21:53:09 +0800

    🔧 build(App): add eleplus plugin

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <router-view />
+  <img src="./assets/logo.png" />
+  <div>
+    <p>
+      If Element Plus is successfully added to this project, you'll see an
+      <code v-text="'<el-button>'"></code>
+      below
+    </p>
+    <el-button type="primary">el-button</el-button>
+  </div>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  margin-top: 60px;
 }
 </style>
