ace1d8411a1d7882ec5ff49ec7d35351ae9f5410
jesonwei <872937094@qq.com>
2023-02-26 01:50:14 +0800

    ✨ feat(layout): css module in setup

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,11 +2,16 @@
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import AppMain from './components/AppMain'
+import variable from '@/style/variable.module.scss'
 </script>
 <template>
   <div class="app-wrapper">
     <!-- 左侧 menu -->
-    <sidebar id="guide-sidebar" class="sidebar-container" />
+    <sidebar
+      id="guide-sidebar"
+      :style="{ backgroundColor: variable.menuBg }"
+      class="sidebar-container"
+    />
     <div class="main-container">
       <div class="fixed-header">
         <!-- 顶部的 navbar -->
@@ -17,4 +22,22 @@ import AppMain from './components/AppMain'
     </div>
   </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/style/mixin.scss';
+// @import '~@/style/variable.module.scss';
+$sideBarWidth: 210px;
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,13 @@ function resolve (dir) {
 }
 // https://cli.vuejs.org/zh/guide/webpack.html#%E7%AE%80%E5%8D%95%E7%9A%84%E9%85%8D%E7%BD%AE%E6%96%B9%E5%BC%8F
 module.exports = defineConfig({
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: '@import "~@/style/variable.module.scss";'
+      }
+    }
+  },
   chainWebpack (config) {
     // 设置 svg-sprite-loader
     config.module
