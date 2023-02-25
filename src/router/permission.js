06f5e8326400b8ada09e2a2f91b0efbe1fb42390
jesonwei <872937094@qq.com>
2023-02-26 03:01:57 +0800

    ✨ feat(Navbar): Navbar compon

--- src/router/permission.js
+++ src/router/permission.js
@@ -2,12 +2,10 @@ import store from '@/store'
 import router from '.'
 const whiteList = ['/login', 'about', '484', 'Not Found']
 router.beforeEach(async (to, from, next) => {
-  console.log(store.getters.token)
   if (store.getters.token) {
     if (to.path === '/login') {
       next('/')
     } else {
-      console.log(store.getters.hasUserInfo)
       // 判断用户资料是否存在
       if (!store.getters.hasUserInfo) {
         await store.dispatch('user/getUserInfo')
--- src/store/module/user.js
+++ src/store/module/user.js
@@ -43,7 +43,6 @@ export default {
     },
     async getUserInfo (context) {
       const res = await getUserInfo()
-      console.log(res)
       this.commit('user/setUserInfo', res)
       return res
     }
