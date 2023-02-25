47f950c5a167aa60863c9566764404981914fdff
jesonwei <872937094@qq.com>
2023-02-25 20:22:13 +0800

    ✨ feat(MobileForm): MobileForm icon&rule

--- src/views/login/component/MobileForm.vue
+++ src/views/login/component/MobileForm.vue
@@ -4,6 +4,7 @@ import { ElMessage } from 'element-plus'
 import { computed, ref } from 'vue'
 import { useStore } from 'vuex'
 import { LoginState } from '../../../constant/LoginState'
+import { validateMobileNumber } from '../rules'
 const store = useStore()
 const formData = ref({
   mobile: '',
@@ -11,6 +12,16 @@ const formData = ref({
 })
 const formRef = ref()
 const loading = ref(false)
+const mobileFormRules = ref({
+  mobile: [
+    {
+      required: true,
+      message: '请输入合法手机号',
+      trigger: 'change',
+      validator: validateMobileNumber()
+    }
+  ]
+})
 const handleReset = async () => {
   ElMessage.warning('暂不支持手机登陆～')
 }
@@ -25,10 +36,17 @@ const handleBackLogin = () => {
   <el-form
     v-if="getShow"
     ref="formRef"
+    class="mobileForm"
+    :rules="mobileFormRules"
     :model="formData"
     @keypress.enter="handleReset"
   >
     <el-form-item class="enter-x" prop="mobile">
+      <span class="svgComponent">
+        <el-icon>
+          <Iphone />
+        </el-icon>
+      </span>
       <el-input
         v-model="formData.mobile"
         placeholder="手机号"
@@ -36,6 +54,9 @@ const handleBackLogin = () => {
       ></el-input>
     </el-form-item>
     <el-form-item class="enter-x count" prop="sms">
+      <span class="svgComponent">
+        <el-icon><ChatLineSquare /></el-icon>
+      </span>
       <CountdownInput
         v-model="formData.sms"
         placeholder="短信验证码"
@@ -61,6 +82,90 @@ const handleBackLogin = () => {
   </el-form>
 </template>
 <style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: rgba(211, 17, 240, 0.7);
+$black: #000;
+.mobileForm {
+  position: relative;
+
+  ::v-deep .el-input {
+    display: inline-block;
+    input {
+      padding: 12px 5px 12px 15px;
+      height: 47px;
+      background: transparent;
+      color: $black;
+      height: 47px;
+      caret-color: $cursor;
+    }
+  }
+  ::v-deep .el-form-item {
+    position: relative;
+    border-radius: 0 4px 4px 0;
+  }
+  ::v-deep .el-form-item__content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    line-height: 32px;
+    font-size: var(--font-size);
+    min-width: 0;
+  }
+  .col-form {
+    ::v-deep .el-form-item__content {
+      display: block;
+    }
+  }
+  .svgComponent {
+    position: relative;
+    padding: 6px 10px 0px 0px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+    .el-icon {
+      font-size: 20px;
+      line-height: 2em;
+    }
+  }
+  .eye {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  svg {
+    width: 1em;
+    height: 1em;
+    cursor: pointer;
+    color: #aaa;
+    &:hover {
+      color: #1890ff;
+    }
+  }
+  .block {
+    width: 100%;
+  }
+  .dividertxt {
+    ::v-deep .el-divider__text {
+      color: #aaa;
+    }
+  }
+  .othersvg {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    svg {
+      cursor: pointer;
+      font-size: 22px;
+      color: #888;
+      &:hover {
+        color: #1890ff;
+      }
+    }
+  }
+}
 .count {
   ::v-deep .el-form-item__content {
     display: flex;
