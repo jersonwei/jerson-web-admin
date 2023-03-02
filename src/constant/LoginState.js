45d594712403cc4a6efd096b87d04e141075da2e
jesonwei <872937094@qq.com>
2023-03-03 01:56:45 +0800

    ✨ feat(store): add lang in store

--- src/constant/LoginState.js
+++ src/constant/LoginState.js
@@ -6,3 +6,4 @@ export const LoginState = {
   QR_CODE: 'QR_CODE'
 }
 export const TOKEN_TIMEOUT_VALUE = 2 * 3600 * 1000
+export const LANG = 'language'
--- src/store/module/app.js
+++ src/store/module/app.js
@@ -1,11 +1,15 @@
 export default {
   namespaced: true,
   state: () => ({
-    sideBarOpen: true
+    sideBarOpen: true,
+    language: 'zh'
   }),
   mutations: {
     setSiderBarState (state, val) {
       state.sideBarOpen = !state.sideBarOpen
+    },
+    setLanguage (state, val) {
+      state.language = val
     }
   },
   actions: {},
