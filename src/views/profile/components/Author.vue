5217dd267e3d1d12f0f822f7e5c6dce27020bb25
jesonwei <872937094@qq.com>
2023-03-05 20:41:20 +0800

    ✨ feat(profile): feature done

--- src/views/profile/components/Author.vue
+++ src/views/profile/components/Author.vue
@@ -1,5 +1,46 @@
-<script setup></script>
 <template>
-  <div></div>
+  <div class="author-container">
+    <div class="header">
+      <pan-thumb :image="imgURl" height="60px" width="60px" :hoverable="false">
+        {{ $t('msg.profile.name') }}
+      </pan-thumb>
+      <div class="header-desc">
+        <h3>{{ $t('msg.profile.name') }}</h3>
+        <span>{{ $t('msg.profile.job') }}</span>
+      </div>
+    </div>
+    <div class="info">
+      {{ $t('msg.profile.Introduction') }}
+    </div>
+  </div>
 </template>
-<style lang="scss" scoped></style>
+
+<script setup>
+import PanThumb from './PanThumb/index.vue'
+import {} from 'vue'
+const imgURl = require('@/assets/ailun.jpeg')
+</script>
+
+<style lang="scss" scoped>
+.author-container {
+  .header {
+    display: flex;
+    .header-desc {
+      margin-left: 12px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+
+      span {
+        font-size: 14px;
+      }
+    }
+  }
+  .info {
+    margin-top: 16px;
+    line-height: 22px;
+    font-size: 14px;
+    text-indent: 26px;
+  }
+}
+</style>
