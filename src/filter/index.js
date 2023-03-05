e523980fe5ef4ba1f85b08d2c237ffb5c5eb2f70
jesonwei <872937094@qq.com>
2023-03-06 05:14:59 +0800

    ✨ feat(article-ranking): diy table col

--- src/filter/index.js
+++ src/filter/index.js
@@ -1,5 +1,16 @@
+import store from '@/store'
 import dayjs from 'dayjs'
-
+import rt from 'dayjs/plugin/relativeTime'
+// 加载相对时间插件
+dayjs.extend(rt)
+function relativeTime (val) {
+  if (!isNaN(val)) {
+    val = parseInt(val)
+  }
+  return dayjs()
+    .locale(store.getters.language === 'zh' ? 'zh-cn' : 'en')
+    .to(dayjs(val))
+}
 const dateFilter = (val, format = 'YYYY-MM-DD') => {
   if (!isNaN(val)) {
     val = parseInt(val)
@@ -10,6 +21,7 @@ const dateFilter = (val, format = 'YYYY-MM-DD') => {
 
 export default app => {
   app.config.globalProperties.$filters = {
-    dateFilter
+    dateFilter,
+    relativeTime
   }
 }
