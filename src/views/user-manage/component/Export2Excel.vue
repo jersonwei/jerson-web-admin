fbb254e15798d95c7fc8f99e47c24dd24cd9057c
jesonwei <872937094@qq.com>
2023-03-05 23:53:26 +0800

    ✨ feat(export2excel): basic feature done

--- src/views/user-manage/component/Export2Excel.vue
+++ src/views/user-manage/component/Export2Excel.vue
@@ -25,6 +25,8 @@ import { watchSwitchLang } from '@/utils/i18n'
 import { defineProps, defineEmits, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { getUserManageAllList } from '@/api/user-manage'
+import { USER_RELATIONS } from './Export2ExcelContent'
+import { dateFilter } from '@/filter/index'
 const i18n = useI18n()
 defineProps({
   modelValue: {
@@ -47,10 +49,43 @@ const loading = ref(false)
 const onConfirm = async () => {
   loading.value = true
   const allUser = (await getUserManageAllList()).list
-  console.log(allUser)
+  // 导入工具包
+  const excel = await import('@/utils/Export2Excel')
+  const data = formatJson(USER_RELATIONS, allUser)
+  excel.export_json_to_excel({
+    // excel 表头
+    header: Object.keys(USER_RELATIONS),
+    // excel 数据（二维数组结构）
+    data,
+    // 文件名称
+    filename: excelName.value || exportDefaultName,
+    // 是否自动列宽
+    autoWidth: true,
+    // 文件类型
+    bookType: 'xlsx'
+  })
   closed()
 }
+// 该方法负责将数组转化成二维数组
+const formatJson = (headers, rows) => {
+  // 首先遍历数组
+  // [{ username: '张三'},{},{}]  => [[’张三'],[],[]]
+  return rows.map(item => {
+    return Object.keys(headers).map(key => {
+      // 角色特殊处理
+      // if (headers[key] === 'role') {
+      //   const roles = item[headers[key]]
 
+      //   return JSON.stringify(roles.map(role => role.title))
+      // }
+      // 时间特殊处理
+      if (headers[key] === 'openTime') {
+        return dateFilter(+item[headers[key]])
+      }
+      return item[headers[key]]
+    })
+  })
+}
 /**
  * 关闭
  */
