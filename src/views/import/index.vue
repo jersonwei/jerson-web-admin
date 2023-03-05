22eb4123db642fce2a293e08c2eeb886e6bee4cf
jesonwei <872937094@qq.com>
2023-03-05 22:12:00 +0800

    ✨ feat(import): import feat done

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,10 +1,40 @@
 <script setup>
 import UploadExcel from '@/components/UploadExcel/index.vue'
+import { USER_RELATIONS } from './utils'
+import { userBatchImport } from '@/api/user-manage'
+import { ElMessage } from 'element-plus'
+import { useRouter } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+const router = useRouter()
+const i18n = useI18n()
 /**
  * 数据解析成功后的会调
  */
-const onSuccess = excelData => {
-  console.log(excelData)
+/**
+ * 数据解析成功之后的回调
+ */
+const onSuccess = async ({ header, results }) => {
+  const updateData = generateData(results)
+  await userBatchImport(updateData)
+  ElMessage.success({
+    message: results.length + i18n.t('msg.excel.importSuccess'),
+    type: 'success'
+  })
+  router.push('/user/manage')
+}
+/**
+ * 筛选数据
+ */
+const generateData = results => {
+  const arr = []
+  results.forEach(item => {
+    const userInfo = {}
+    Object.keys(item).forEach(key => {
+      userInfo[USER_RELATIONS[key]] = item[key]
+    })
+    arr.push(userInfo)
+  })
+  return arr
 }
 </script>
 <template>
